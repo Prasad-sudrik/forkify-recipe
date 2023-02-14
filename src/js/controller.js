4fe3f8763025ce2baa515ee66344f497d13dcf45
Prasad Sudrik <sudrikprasad90@gmail.com>
2023-02-14 12:35:24 +0530

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -138,6 +138,10 @@ const controlAddRecipe = async function(newRecipe){
 
 ///////////////////////////////////////
 // controlRecipes();
+const newFeature = function(){
+  console.log('Welcome to the application');
+}
+
 const init = function () { 
   bookmarksView.addHandlerRender(controlBookmarks) ;
   recipeView.addHandlerRender(controlRecipes);
@@ -146,7 +150,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  // alert('HACKED');
+  newFeature();
 }
 init();
 
