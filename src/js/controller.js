b8e70c211b22c75a17fa67366ffe692a662da6a1
Prasad Sudrik <sudrikprasad90@gmail.com>
2023-02-14 12:30:33 +0530

    New feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -146,6 +146,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  alert('HACKED');
 }
 init();
 
